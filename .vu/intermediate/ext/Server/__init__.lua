f265f564cd38238ece97496ceb9e88a9f2ea8fe9
FlashHit <56718716+FlashHit@users.noreply.github.com>
2021-07-04 18:47:43 +0200

    remove .vu files

--- .vu/intermediate/ext/Server/__init__.lua
+++
@@ -1,9 +0,0 @@
---[[
-    Venice Unleashed - Intermediate Lua binding
-    Type: ext\Server\__init__.lua
-    Generated on: Sun Jul 04 2021 18:42:39 GMT+0200 (Mitteleuropäische Sommerzeit)
---]]
-
----@meta
-
-
--- .vu/intermediate/ext/Shared/__init__.lua
+++
@@ -1,9 +0,0 @@
---[[
-    Venice Unleashed - Intermediate Lua binding
-    Type: ext\Shared\__init__.lua
-    Generated on: Sun Jul 04 2021 18:46:09 GMT+0200 (Mitteleuropäische Sommerzeit)
---]]
-
----@meta
-
-
