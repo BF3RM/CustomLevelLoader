9d83a3593fcdfea83e49e8c0016866e31f86ad47
FlashHit <56718716+FlashHit@users.noreply.github.com>
2022-07-20 00:51:09 +0200

    feat(Config): add RM mirror back

--- ext/Shared/Config.lua
+++ ext/Shared/Config.lua
@@ -1,6 +1,8 @@
 return {
-	USE_HTTP = false,
-	MIRRORS = {},
+	USE_HTTP = true,
+	MIRRORS = {
+		"https://s3.bf3reality.com/assets/customlevelloader/"
+	},
 	LOGGER_ENABLED = false,
 	CLIENT_TIMEOUT = 50
 }
